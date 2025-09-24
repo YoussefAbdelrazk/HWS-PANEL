cc51967169e16393fee8cd768e29c191fb6c8d39
YoussefAbdelrazk <youssefabdelrazk2002@gmail.com>
2025-09-24 23:33:05 +0300

    feat : add structure of project

---
+++ src/app/error.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function error() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <h1>Error</h1>
+
+    </div>
+  )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function notFound() {
+  return (
+    <div className='flex justify-center items-center h-screen w-screen'>
+      <h1>Not Found</h1>
+    </div>
+  );
+}
